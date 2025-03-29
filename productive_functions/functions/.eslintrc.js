342cb18c180e766575d89f86efe315333b7c8e3c
erluxman <erluxman@gmail.com>
2025-03-29 11:16:32 +0545

    added some dox to catch running app's bug fast

--- productive_functions/functions/.eslintrc.js
+++ productive_functions/functions/.eslintrc.js
@@ -10,10 +10,19 @@ module.exports = {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    quotes: ["error", "double", {allowTemplateLiterals: true}],
-    "object-curly-spacing": ["error", "never"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "object-curly-spacing": ["error", "always"],
+    "object-curly-newline": [
+      "error",
+      {
+        multiline: true,
+        consistent: true,
+      },
+    ],
     indent: ["error", 2],
-    "max-len": ["error", {code: 100}],
+    "max-len": ["error", { code: 100 }],
+    "comma-dangle": ["error", "always-multiline"],
+    "no-multiple-empty-lines": ["error", { max: 1 }],
   },
   overrides: [
     {
---
+++ productive_functions/readme.md
@@ -0,0 +1,11 @@
+# Productive Firebase Function setup
+
+## Cloud Run
+
+## Local Run
+
+`firebase emulators:start --only functions,firestore --project`
+
+## Common Errors & Troubleshooting
+
+1. when we see the error "Header name must be a valid HTTP token ["* import function triggers from their respective submodules"]", we need to reload the window from command palette
